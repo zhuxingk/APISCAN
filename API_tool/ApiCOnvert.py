14faec34848f2ccb31f247c2c5f9f9dff791cc83
zhuxingk <2677748098@qq.com>
2023-05-11 17:10:31 +0800

    修复SQL语句输出错误问题；

--- API_tool/ApiCOnvert.py
+++ API_tool/ApiCOnvert.py
@@ -1,3 +1,4 @@
+import json
 import re
 from bs4 import BeautifulSoup
 
@@ -26,7 +27,7 @@ class MarkdowntoSql:
                     self.sql_list.append(self.generate_sql(headers, values))
 
         # 解析markdown标题和内容
-        sections = re.findall('##\s(.*?)\n([\s\S]*?)(?=##\s|$)', md)
+        sections = re.findall('^#{2}\s+(.*?)\n([\s\S]*?)(?=^#{2}\s+|\Z)', md)
 
         for title, content in sections:
             url = ''
@@ -43,15 +44,31 @@ class MarkdowntoSql:
                 elif line.startswith('- Method:'):
                     method = line.replace('- Method:', '').strip()
                 elif line.startswith('- Request:'):
-                    request = self.parse_json(lines, 'Request')
+                    request_lines = []
+                    index = lines.index(line) + 1
+                    while index < len(lines) and not lines[index].startswith('- Response'):
+                        request_lines.append(lines[index])
+                        index += 1
+                    request = self.parse_json(request_lines, 'Request')
                 elif line.startswith('- Response(TRUE):'):
-                    response_true = self.parse_json(lines, 'Response(TRUE)')
+                    response_true_lines = []
+                    index = lines.index(line) + 1
+                    while index < len(lines) and not lines[index].startswith('- Response'):
+                        response_true_lines.append(lines[index])
+                        index += 1
+                    response_true = self.parse_json(response_true_lines, 'Response(TRUE)')
                 elif line.startswith('- Response(FALSE):'):
-                    response_false = self.parse_json(lines, 'Response(FALSE)')
+                    response_false_lines = []
+                    index = lines.index(line) + 1
+                    while index < len(lines) and not lines[index].startswith('- Response'):
+                        response_false_lines.append(lines[index])
+                        index += 1
+                    response_false = self.parse_json(response_false_lines, 'Response(FALSE)')
 
             # 根据解析到的值生成sql语句
             self.sql_list.append(self.generate_sql(title, url, method, request, response_true, response_false))
-            print(self.sql_list)
+
+        print(self.sql_list)
 
     def generate_sql(self, *args):
         sql = "INSERT INTO `api` ("
@@ -75,40 +92,26 @@ class MarkdowntoSql:
         sql = sql[:-2] + ");\n"
         return sql
 
-    # def generate_sql(self, *args):
-    #     sql = "INSERT INTO `api` ("
-    #     for arg in args:
-    #         if type(arg) == str:
-    #             sql += "`title`, "
-    #         elif type(arg) == list:
-    #             for item in arg:
-    #                 sql += f"`{item}`, "
-    #         else:
-    #             sql += "`url`, `method`, `request`, `response_true`, `response_false`"
-    #             break
-    #     sql = sql[:-2] + ") VALUES ("
-    #     for arg in args:
-    #         if type(arg) == str:
-    #             sql += f"'{arg}', "
-    #         elif type(arg) == list:
-    #             sql += f"'{','.join(arg)}', "
-    #         else:
-    #             sql += f"'{arg}', "
-    #     sql = sql[:-2] + ");\n"
-    #     return sql
-
-    def parse_json(self, lines, start):
-        result = ''
-        flag = False
+    def parse_json(self, lines, key):
+        """
+        解析JSON字符串
+        """
+        json_str = ''
+        start = False
         for line in lines:
-            if line.startswith(f'- {start}:'):
-                result += line.replace(f'- {start}:', '').strip()
-                flag = True
-            elif flag and line.startswith('- '):
-                result += '\n' + line[2:].strip()
-            else:
+            if line.startswith(f'- {key}:'):
+                start = True
+            elif start and line.startswith('-'):
                 break
-        return result
+            elif start:
+                json_str += line.strip() + '\n'
+
+        # 处理json字符串
+        try:
+            json_obj = json.loads(json_str)
+            return json.dumps(json_obj, ensure_ascii=False)
+        except:
+            return ''
 
     def decode_sql(self):
         self.parse_markdown()
