11cb8e04007e4b596c5e6ef04926363634a9cafb
zhuxingk <2677748098@qq.com>
2023-05-19 17:37:25 +0800

    CLI类完善；

--- API_ScanTool/CLI.py
+++ API_ScanTool/CLI.py
@@ -13,22 +13,27 @@ class CLI:
         self.log_manager = LogManager('api_scan_tool.log')
 
     def run(self):
-        self.log_manager.log_debug('CLI run() method started.')
-        parser = self.create_parser()
-        args = parser.parse_args()
+        self.log_manager.log_debug('API Scan Tool started.')
+        while True:
+            self.print_menu()
+            choice = input('Enter your choice: ')
 
-        if args.default:
-            self.execute_all_apis()
-        elif args.name:
-            self.execute_selected_api(args.name)
+            if choice == '1':
+                self.execute_all_apis()
+            elif choice == '2':
+                self.execute_selected_api()
+            elif choice.lower() == 'q':
+                break
+            else:
+                print('Invalid choice. Please try again.')
 
-    def create_parser(self):
-        self.log_manager.log_debug('Executing all APIs.')
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-d', '--default', action='store_true', help='Execute all APIs')
-        parser.add_argument('-n', '--name', help='API name')
-        # args = parser.parse_args()
-        return parser
+        self.log_manager.log_debug('API Scan Tool finished.')
+
+    def print_menu(self):
+        print('--- API Scan Tool Menu ---')
+        print('1. Default Test')
+        print('2. Test by API Name')
+        print('q. Quit')
 
     def execute_all_apis(self):
         api_names = self.sender.get_api_names()
@@ -36,39 +41,62 @@ class CLI:
             result = self.choice.execute_selected_api(api_name)
             self.process_result(api_name, result)
 
-    def execute_selected_api(self, api_name):
-        result = self.choice.execute_selected_api(api_name)
-        self.process_result(api_name, result)
+    def execute_selected_api(self):
+        while True:
+            api_names = self.sender.get_api_names()
+            print('Available API Names:')
+            for i, api_name in enumerate(api_names, 1):
+                print(f'{i}. {api_name}')
+
+            choice = input('Enter the number corresponding to the API you want to test: ')
+            if choice.isdigit() and 1 <= int(choice) <= len(api_names):
+                api_name = api_names[int(choice) - 1]
+                result = self.choice.execute_selected_api(api_name)
+                self.process_result(api_name, result)
+            elif choice.lower() == 'b':
+                return
+            elif choice.lower() == 'q':
+                quit()
+            else:
+                print('Invalid choice. Please try again.')
+
+            while True:
+                next_action = input("Choose the next action: (C)continue, (B)back, (Q)quit: ")
+                if next_action.lower() == 'c':
+                    break
+                elif next_action.lower() == 'b':
+                    break
+                elif next_action.lower() == 'q':
+                    quit()
+                else:
+                    print('Invalid choice. Please try again.')
 
     def process_result(self, api_name, result):
-        if 'error' in result:
-            error_message = f"API '{api_name}' execution failed. Error: {result['error']}"
-            self.log_manager.log_error(error_message)
-        else:
-            status_code = result['status_code']
-            response = result['response']
-            success_message = f"API '{api_name}' executed successfully. Status code: {status_code}"
-            self.log_manager.log_info(success_message)
-            self.log_manager.log_debug(f"Response: {response}")
+        try:
+            if 'error' in result:
+                error_message = f"API '{api_name}' execution failed. Error: {result['error']}"
+                self.log_manager.log_error(error_message)
+            else:
+                status_code = result['status_code']
+                response = result['response']
+                success_message = f"API '{api_name}' executed successfully. Status code: {status_code}"
+                self.log_manager.log_info(success_message)
+                self.log_manager.log_debug(f"Response: {response}")
 
-        api_info = self.choice.collection.find_one({'name': api_name})
-        url = api_info['URL']
-        method = api_info['Method']
-        request = api_info['Request']
-        self.sender.save_response(api_name, url, method, request, status_code, response)
+            api_info = self.choice.collection.find_one({'name': api_name})
+            if api_info:
+                url = api_info['URL']
+                method = api_info['Method']
+                request = api_info['Request']
+                self.sender.save_response(api_name, url, method, request, status_code, response)
+            else:
+                print(f"API '{api_name}' not found.")
+        except Exception as e:
+            error_message = f"An error occurred while processing the result of API '{api_name}'. Error: {str(e)}"
+            self.log_manager.log_error(error_message)
 
 
 if __name__ == '__main__':
     mongodb_url = 'mongodb://localhost:27017/'
     database_name = 'APIInfo'
     CLI(mongodb_url, database_name).run()
-    log_manager = LogManager('api_scan_tool.log')
-    log_manager.log_debug('API Scan Tool started.')
-
-    CLI(mongodb_url, database_name).run()
-
-    log_manager.log_debug('API Scan Tool finished.')
-
-
-
-
